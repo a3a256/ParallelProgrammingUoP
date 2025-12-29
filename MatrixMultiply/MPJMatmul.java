eda05cfee5448562f8241cf1dd1b3966ddcfdedb
a3amat1K <81564718+a3a256@users.noreply.github.com>
2025-12-29 16:26:48 +0000

    uploaded MPJMatmul changes that divides operation for both matrices

--- MatrixMultiply/MPJMatmul.java
+++ MatrixMultiply/MPJMatmul.java
@@ -1,5 +1,12 @@
 import mpi.* ;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.sun.nio.sctp.SendFailedNotification;
+
 public class MPJMatmul {
     public static final int N = 1024 ;
     static int me, P, B;
@@ -17,21 +24,39 @@ public class MPJMatmul {
         int processes_per_matrix = (int)Math.sqrt(P);
 
         int block_size = N/processes_per_matrix;
+        int q = processes_per_matrix;
 
-        a = new float[block_size*N];
-        b = new float[block_size*N];
+        int blockRow = me / q;
+        int blockCol = me % q;
+
+        a = new float[N*N];
+        b = new float[N*N];
         c = new float[block_size*block_size];
+        float [] rec_c = new float[N*N];
 
-        int i, j, k;
+        int i, j, k, prev;
 
         float sum = 0.0f;
 
-        for(i=0; i<block_size; i++){
-            for(j=0; j<N; j++){
-                a[block_size*i + j] = (float)Math.random();
-                b[block_size*i + j] = (float)Math.random();
+        long startTime = System.currentTimeMillis() ;
+
+        if(me == 0){
+
+            for(i=0; i<N; i++){
+                for(j=0; j<N; j++){
+                    a[N*i + j] = (float)Math.random();
+                    b[N*i + j] = (float)Math.random();
+                    rec_c[N*i + j] = (float)(N*i + j);
+                }
             }
+            // for(i=1; i<P; i++){
+            //     MPI.COMM_WORLD.Send(a, 0, N*N, MPI.FLOAT, i, 0);
+            //     MPI.COMM_WORLD.Send(b, 0, N*N, MPI.FLOAT, i, 0);
+            // }
         }
+
+        MPI.COMM_WORLD.Bcast(a, 0, N * N, MPI.FLOAT, 0);
+        MPI.COMM_WORLD.Bcast(b, 0, N * N, MPI.FLOAT, 0);
 
         for(i=0; i<block_size; i++){
             for(j=0; j<block_size; j++){
@@ -39,43 +64,39 @@ public class MPJMatmul {
             }
         }
 
-        long startTime = System.currentTimeMillis() ;
-
         for(i=0; i<block_size; i++){
+            int globalRow = blockRow * block_size + i;
             for(j=0; j<block_size; j++){
+                int globalCol = blockCol * block_size + j;
                 sum = 0.0f;
                 for(k=0; k<N; k++){
-                    sum += a[block_size*i + k]*b[block_size*k + j];
+                    // sum += a[(block_size*i + ((me/q) * block_size)) + k]*b[N*k + (j+block_size*me%q)];
+                    sum += a[globalRow*N + k]*b[k * N + globalCol];
                 }
                 c[block_size * i + j] += sum;
             }
         }
 
+
         if(me>0){
             MPI.COMM_WORLD.Send(c, 0, block_size*block_size, MPI.FLOAT, 0, 0) ;
         }else{
-            float [] rec_c = new float[N*N];
             for(i=0; i<block_size; i++){
                 for(j=0; j<block_size; j++){
-                    rec_c[block_size*i + j] = c[block_size*i + j];
+                    rec_c[N*i + j] = c[block_size*i + j];
+                    // System.out.println("Val id in rec: " + (N*i + j) + "\nReceived: " + rec_c[N*i + j] + " original: " + c[block_size*i + j]);
                 }
             }
-            int row_start, row_end, col_start, col_end;
             for(int src = 1; src<P; src++){
-                System.out.println(src);
+                // System.out.println(src);
                 MPI.COMM_WORLD.Recv(c, 0, block_size*block_size, MPI.FLOAT, src, 0) ;
                 for(i=0; i<block_size; i++){
+                    int globalRow = (src/q) * block_size + i;
                     for(j=0; j<block_size; j++){
-                        if(i == 0 && j == 0){
-                            System.out.print("Row " + (block_size*i + block_size*(src/2)) + " Col " + (j+block_size*(src%2)) + " ");
-                        }else if(i == 0 && j == block_size-1){
-                            System.out.println("Row " + (block_size*i + block_size*(src/2)) + " Col " + (j+block_size*(src%2)));
-                        }else if(i == block_size - 1 && j == 0){
-                            System.out.print("Row " + (block_size*i + block_size*(src/2)) + " Col " + (j+block_size*(src%2)) + " ");
-                        }else if(i == block_size-1 && j == block_size-1){
-                            System.out.println("Row " + (block_size*i + block_size*(src/2)) + " Col " + (j+block_size*(src%2)));
-                        }
-                        rec_c[(block_size*i + block_size*(src/2)) + (j+block_size*(src%2))] = c[block_size*i + j];
+                        int globalCol = (src%q) * block_size + j;
+                        // System.out.println("Val id in rec: " + ((N*i + block_size*(src/q)) + (j+N*(src%q))));
+                        rec_c[globalRow * N + globalCol] = c[block_size*i + j];
+                        // System.out.println("Received: " + rec_c[(N*i + block_size*(src/q)) + (j+N*(src%q))] + " original: " + c[block_size*i + j]);
                     }
                 }
             }
@@ -85,19 +106,47 @@ public class MPJMatmul {
        
         long timeMs = endTime - startTime ;
 
+        int count_mistakes = 0;
+
+        if(me == 0){
+
+            // for(i=0; i<N; i++){
+            //     for(j=0; j<N; j++){
+            //         System.out.print((N*i + j) + " - " + rec_c[N * i + j] + ", ");
+            //     }
+            //     System.out.print("\n");
+            // }
+
+
+            for(i = 0 ; i < N; i++) {
+                for(j = 0 ; j < N ; j++) {
+                    sum = 0.0f;
+                    for(k = 0 ; k < N ; k++) {
+                        sum += a [N * i + k] * b [N * k + j] ;
+                    }
+
+                    // System.out.print((N*i + j) + " - " + sum + ", ");
+                    if (Math.abs(rec_c[N*i + j] - sum) > 1e-3){
+                        // if(count_mistakes % 1000 == 0){
+                        //     System.out.println("Mistake found at (" + N*i + "," + j +")");
+                        //     System.out.println("Expected to see " + rec_c[N * i + j] + " but found " + sum);
+                        // }
+                        count_mistakes += 1;
+                    }
+                }
+                // System.out.print("\n");
+            }
+        }
+
         if(me == 0){
 
             System.out.println("MPJ matrix multiplication completed in "
                     + timeMs + " milliseconds") ;
+
+            System.out.println("Number of mistakes: " + count_mistakes);
         }
 
         MPI.Finalize() ;
 
     }
 }
-
-
-
-// in order to compile
-
-// run: javac -cp .:$MPJ_HOME/lib/mpj.jar MPJMatmul.java
