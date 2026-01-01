db5bc5ee9e0accc80d68540c406c9fb2a899397c
a3amat1K <81564718+a3a256@users.noreply.github.com>
2026-01-01 23:01:59 +0000

    Added multithreading in Java for matmul
    The A version separates the A matrix into N block, if N=2 - two block 512x1024
    The B version separates the B matrix into N blocks, if N = 2 - two blocks of size 1024x512

---
+++ MatrixMultiply/ParallelMatmulA.java
@@ -0,0 +1,66 @@
+public class ParallelMatmulA extends Thread{
+
+    public static final int N = 1024 ;
+
+    static float[] a = new float[N*N];
+    static float[] b = new float[N*N];
+    static float[] c = new float[N*N];
+
+    final static int P = 2;
+
+    public static void main(String [] args) throws Exception{
+
+        int i, j;
+
+        for (i = 0 ; i < N ; i++) {
+            for(j = 0 ; j < N ; j++) {
+                a [N * i + j] = i + j ;
+                b [N * i + j] = i - j ;
+            }
+        }
+
+        long startTime = System.currentTimeMillis() ;
+
+        ParallelMatmulA [] threads = new ParallelMatmulA [P] ;
+
+        for(i=0; i<P; i++){
+            threads[i] = new ParallelMatmulA(i);
+            threads[i].start();
+        }
+
+        for(i=0; i<P; i++){
+            threads[i].join();
+        }
+       
+        long endTime = System.currentTimeMillis() ;
+       
+        long timeMs = endTime - startTime ;
+       
+        System.out.println("Sequential matrix multiplication completed in "
+                + timeMs + " milliseconds") ;
+        System.out.println("Sequential performance = " +
+                ((2L * N * N * N) / (1E6 * timeMs)) + " GFLOPS") ;
+    }
+
+    int row;
+
+    public ParallelMatmulA(int row){
+        this.row = row;
+    }
+
+    public void run(){
+        int step = N/P;
+        int start_row = row*step;
+        int end_row = row+step;
+        int i, j;
+        for(i = start_row ; i < end_row; i++) {
+            for(j = 0 ; j < N ; j++) {
+                float sum = 0 ;
+                for(int k = 0 ; k < N ; k++) {
+                    sum += a [N * i + k] * b [N * k + j] ;
+                }
+                c [N * i + j] = sum ;
+            }
+        }
+    }
+}
---
+++ MatrixMultiply/ParallelMatmulB.java
@@ -0,0 +1,66 @@
+public class ParallelMatmulB extends Thread{
+
+    public static final int N = 1024 ;
+
+    static float[] a = new float[N*N];
+    static float[] b = new float[N*N];
+    static float[] c = new float[N*N];
+
+    final static int P = 2;
+
+    public static void main(String [] args) throws Exception{
+
+        int i, j;
+
+        for (i = 0 ; i < N ; i++) {
+            for(j = 0 ; j < N ; j++) {
+                a [N * i + j] = i + j ;
+                b [N * i + j] = i - j ;
+            }
+        }
+
+        long startTime = System.currentTimeMillis() ;
+
+        ParallelMatmulB [] threads = new ParallelMatmulB [P] ;
+
+        for(i=0; i<P; i++){
+            threads[i] = new ParallelMatmulB(i);
+            threads[i].start();
+        }
+
+        for(i=0; i<P; i++){
+            threads[i].join();
+        }
+       
+        long endTime = System.currentTimeMillis() ;
+       
+        long timeMs = endTime - startTime ;
+       
+        System.out.println("Sequential matrix multiplication completed in "
+                + timeMs + " milliseconds") ;
+        System.out.println("Sequential performance = " +
+                ((2L * N * N * N) / (1E6 * timeMs)) + " GFLOPS") ;
+    }
+
+    int row;
+
+    public ParallelMatmulB(int row){
+        this.row = row;
+    }
+
+    public void run(){
+        int step = N/P;
+        int start_row = row*step;
+        int end_row = row+step;
+        int i, j;
+        for(i = 0 ; i < N; i++) {
+            for(j = start_row ; j < end_row ; j++) {
+                float sum = 0 ;
+                for(int k = 0 ; k < N ; k++) {
+                    sum += a [N * i + k] * b [N * k + j] ;
+                }
+                c [N * i + j] = sum ;
+            }
+        }
+    }
+}
